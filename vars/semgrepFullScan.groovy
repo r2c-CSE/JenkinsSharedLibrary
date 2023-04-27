d6a80589e243f585bec37f64276276c2eeadfe0f
Sebastian Revuelta <sebastianrevuelta@gmail.com>
2023-04-27 13:51:09 +0200

    Update semgrepFullScan.groovy

--- vars/semgrepFullScan.groovy
+++ vars/semgrepFullScan.groovy
@@ -2,6 +2,7 @@ def call() {
   sh '''docker pull returntocorp/semgrep && \
             docker run \
             -e SEMGREP_APP_TOKEN=$SEMGREP_APP_TOKEN \
+            -e SEMGREP_REPO_NAME=$SEMGREP_REPO_NAME \
             -v "$(pwd):$(pwd)" --workdir $(pwd) \
             returntocorp/semgrep semgrep ci '''
 }
